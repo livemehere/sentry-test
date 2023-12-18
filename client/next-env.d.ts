ed9568ca2fd8393bb96e1a9f96500ad1abba81d1
taemin_Kong <rhdxoals@nexon.co.kr>
2023-12-18 11:14:37 +0900

    add image import

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -3,3 +3,20 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module "*.svg" {
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  const content: {
+    src: string;
+    width: number;
+    height: number;
+    blurWidth: number;
+    blurHeight: number;
+  };
+
+  export default content;
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import YFLogo from "@public/yf-logo.svg";
+
 export default function Index() {
   return (
     <div>
       <img src="/yf-logo.svg" alt="" />
+      <img src={YFLogo.src} alt="" />
     </div>
   );
 }
